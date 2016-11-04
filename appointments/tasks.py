2bb12266b6fdd164759d4f2617f6e57359ff0257
Robin Miguel Anupol <ranupol@gmail.com>
2016-11-04 08:34:29 +0800

    Disable time check for now....

--- appointments/tasks.py
+++ appointments/tasks.py
@@ -55,22 +55,23 @@ def deliver_message(appointment_message_id):
         return
     # appointment_message = appointment.messages.create(template=message_template)
     # check time
-    if timezone.now() < appointment_message.scheduled_delivery_datetime:
-        # don't send/ skip message
-        logger.info("skipping message")
-        pass
-    else:
-        logger.info("sending message")
-        message = client.messages.create(
-            body=message_template.content.format(**appointment.get_data()),
-            to=appointment_message.recipient.patient_phone,
-            from_=settings.TWILIO_NUMBER,
-            # Add callback to the thing
-        )
-        appointment_message.twilio_status = 'delivered'
-        appointment_message.save()
-        logger.info(message.sid)
-        # TODO set a field in appointment_message as id from twilio
+    logger.info("times:\t{}\t{}".format(timezone.now(), appointment_message.scheduled_delivery_datetime))
+    # if timezone.now() < appointment_message.scheduled_delivery_datetime:
+    #     # don't send/ skip message
+    #     logger.info("skipping message")
+    #     pass
+    # else:
+    logger.info("sending message")
+    message = client.messages.create(
+        body=message_template.content.format(**appointment.get_data()),
+        to=appointment_message.recipient.patient_phone,
+        from_=settings.TWILIO_NUMBER,
+        # Add callback to the thing
+    )
+    appointment_message.twilio_status = 'delivered'
+    appointment_message.save()
+    logger.info(message.sid)
+    # TODO set a field in appointment_message as id from twilio
     appointment.schedule_next_message()
     # next_message = appointment.get_next_template()
     # deliver_message.apply_async((appointment_id, next_message.id), eta=TODOtime)
